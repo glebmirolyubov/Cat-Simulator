9f5fa975ceea5c6a1cfb1039168b388d4da686c2
Gleb Mirolyubov <gleb.mirolyubov@gmail.com>
2020-08-18 19:51:58 +0600

    Added animation event to clear reaction text

---
+++ Cat Simulator/Assets/Scripts/Other/ClearReactionText.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ClearReactionText : MonoBehaviour
+{
+    public Text reactionText;
+
+    public void ClearText()
+    {
+        reactionText.text = "";
+    }
+}
